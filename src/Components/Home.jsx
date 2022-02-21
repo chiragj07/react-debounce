867e54a90884156e953d814af78e6b7f27de2c73
chiragj07 <joshi.chirag54@gmail.com>
2022-02-21 23:32:01 +0530

    intersection observer added on inifinite scrolling

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState ,useRef } from 'react'
 // import useDebounce from '../hooks/useDebounce';
 const Home = () => {
 
     const [dbs, setDbs] = useState("");
     const [books ,setBooks]= useState([]);
     const [page, setPage] = useState(1)
-    const debounce = (func, delay)=>{
+    const [loading, setLoading] = useState(true);
+    const [hasMore, setHasMore] = useState(true);
+        const debounce = (func, delay)=>{
         let timer;
         return function(...args){
             if(timer) clearTimeout(timer);
@@ -18,12 +20,36 @@ const Home = () => {
     },[dbs]);
 
     useEffect(() => {
-        fetch(`http://openlibrary.org/search.json?q=${dbs}&page=${page}`).then(res=> res.json()).then(data=> setBooks(pre=>[...new Set([...pre, ...data.docs.map(book=>book.title)])])).catch(err=> console.log(err))
+        setLoading(true)
+        fetch(`http://openlibrary.org/search.json?q=${dbs}&page=${page}`).then(res=> res.json()).then(data=> {  
+             console.log(data);
+             setHasMore(data.docs.length > 0);
+            setBooks(pre=>[...new Set([...pre, ...data.docs.map(book=>book.title)])])
+            console.log(books.length)
+            setLoading(false);
+        }).catch(
+                err=> {
+                    setLoading(false);
+                    console.log(err)
+                    return <><div>ERROR</div></>
+                   
+                })
       
     }, [dbs,page])
     
+    const observer = useRef();
+    const lastElement = useCallback((node)=>{
+            if(loading) return ;
+            if(observer.current) observer.current.disconnect();
+            
+            observer.current = new IntersectionObserver((entries)=>{
+                    if(entries[0].isIntersecting && hasMore) setPage(page => page+1);
+            });
+            if(node) observer.current.observe(node);
 
 
+
+    },[loading, hasMore]);
     const db = debounce((e)=>setDbs(e.target.value),1000);
     
      return (
@@ -31,8 +57,11 @@ const Home = () => {
      <input type="text"  onChange= {db} />
      <div>
          {
-            books.length >0 && books.map((book)=><h2 key={book}>{book}</h2>)
-         }
+            books.length > 0 && books.map((book, index)=>{return (
+            index+1=== books.length ? <h2 key={book} ref = {lastElement}>{book}</h2> :
+            <h2 key={book}>{book}</h2>)}) 
+          }
+    <div> {loading && "Loading" }</div> 
      </div>
 
     </>
